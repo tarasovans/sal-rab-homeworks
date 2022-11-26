1b986b56756e89676d267f88d57eced24a8faefb
nata <tarsovans@gmail.com>
2022-11-26 19:58:18 +0300

    Final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,22 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = { client: "", order: {}, goods: []};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i++) {
+       data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.client = name + " " + phone;
 
-    data.client = 'Иван';
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    //data.order.address = address;
+    data.order.sum = sum;
 
-    let jsonData = JSON.stringify(data);
+
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
